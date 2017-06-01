c141bab5d88b52355c0cd5cfa1ee9adb7d64b6ac
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2017-06-01 18:42:21 +0900

    Change csv script

--- Assets/Scripts/CSVReader.cs
+++ Assets/Scripts/CSVReader.cs
@@ -4,16 +4,6 @@ using System.Linq;
 
 public class CSVReader : MonoBehaviour 
 {
-	//public TextAsset csvFile; 
-	public void Start()
-	{
-		var csvFile = Resources.Load("CSV/Character.csv") as TextAsset;
-		string[,] grid = SplitCsvGrid(csvFile.text);
-		Debug.Log ("size = " + (1 + grid.GetUpperBound (0)) + "," + (1 + grid.GetUpperBound (1)));
-
-		DebugOutputGrid(grid); 
-	}
-
 	// splits a CSV file into a 2D string array
 	static public string[,] SplitCsvGrid(string csvText)
 	{
@@ -34,14 +24,13 @@ public class CSVReader : MonoBehaviour
 			string[] row = SplitCsvLine( lines[y] ); 
 			for (int x = 0; x < row.Length; x++) 
 			{
-				outputGrid[x,y] = row[x]; 
+				outputGrid[x,y] = row[x];
 
 				// This line was to replace "" with " in my output. 
 				// Include or edit it as you wish.
 				outputGrid[x,y] = outputGrid[x,y].Replace("\"\"", "\"");
 			}
 		}
-
 		return outputGrid; 
 	}
 
@@ -52,20 +41,5 @@ public class CSVReader : MonoBehaviour
 			@"(((?<x>(?=[,\r\n]+))|""(?<x>([^""]|"""")+)""|(?<x>[^,\r\n]+)),?)", 
 			System.Text.RegularExpressions.RegexOptions.ExplicitCapture)
 			select m.Groups[1].Value).ToArray();
-	}
-
-	// outputs the content of a 2D array, useful for checking the importer
-	static public void DebugOutputGrid(string[,] grid)
-	{
-		string textOutput = ""; 
-		for (int y = 0; y < grid.GetUpperBound(1); y++) {	
-			for (int x = 0; x < grid.GetUpperBound(0); x++) {
-
-				textOutput += grid[x,y]; 
-				textOutput += "|"; 
-			}
-			textOutput += "\n"; //"\nは改行の意味"
-		}
-		Debug.Log(textOutput);
 	}
 }
