fd926c2651d6aa9797727adef0ebc8dd6b3fe5ac
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2017-06-01 18:46:41 +0900

    Change cdv

--- Assets/Scripts/CSVReader.cs
+++ Assets/Scripts/CSVReader.cs
@@ -4,19 +4,6 @@ using System.Linq;
 
 public class CSVReader : MonoBehaviour 
 {
-<<<<<<< HEAD
-=======
-	//public TextAsset csvFile; 
-	public void Start()
-	{
-		//var csvFile = Resources.Load("CSV/Character.csv") as TextAsset;
-		//string[,] grid = SplitCsvGrid(csvFile.text);
-		//Debug.Log ("size = " + (1 + grid.GetUpperBound (0)) + "," + (1 + grid.GetUpperBound (1)));
-
-		//DebugOutputGrid(grid); 
-	}
-
->>>>>>> 9b47ed9d147f425b1d3a0b72a0d3536c2eaf0147
 	// splits a CSV file into a 2D string array
 	static public string[,] SplitCsvGrid(string csvText)
 	{
@@ -55,23 +42,4 @@ public class CSVReader : MonoBehaviour
 			System.Text.RegularExpressions.RegexOptions.ExplicitCapture)
 			select m.Groups[1].Value).ToArray();
 	}
-<<<<<<< HEAD
-=======
-
-
-	// outputs the content of a 2D array, useful for checking the importer
-	//static public void DebugOutputGrid(string[,] grid)
-	//{
-	//	string textOutput = ""; 
-	//	for (int y = 0; y < grid.GetUpperBound(1); y++) {	
-	//		for (int x = 0; x < grid.GetUpperBound(0); x++) {
-	//
-	//			textOutput += grid[x,y]; 
-	//			textOutput += "|"; 
-	//		}
-	//		textOutput += "\n"; //"\nは改行の意味"
-	//	}
-	//	Debug.Log(textOutput);
-	//}
->>>>>>> 9b47ed9d147f425b1d3a0b72a0d3536c2eaf0147
 }
